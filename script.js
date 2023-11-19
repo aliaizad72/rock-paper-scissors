a335087a5e902598794c1eb5960649b283cd3dc1
Ali Aizad <aliaizad72@gmail.com>
2023-11-19 16:47:30 +0800

    Able to play one game in the console

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// function getComputerChoice () {
-//     const rps = ["rock", "paper", "scissors"];  
-//     const pick = Math.floor(Math.random()*rps.length); 
-//     return rps[pick]; 
-// }
-
 const gameButton = document.querySelector('#game-button');
 
 gameButton.addEventListener('click', startGame);
 
-function deleteNode() {
-    gameButton.remove();
+function deleteNode(event) {
+    event.target.remove();
+}
+
+function getComputerChoice () {
+    const rps = ["rock", "paper", "scissors"];  
+    const pick = Math.floor(Math.random()*rps.length); 
+    return rps[pick]; 
 }
 
 function createButtons() {
@@ -36,12 +36,37 @@ function createButtons() {
     restartDiv.appendChild(restart);
 }
 
-function createScoreboard () {
-}
 
-
-function startGame() {
-    deleteNode();
+function startGame(event) {
+    deleteNode(event);
     createButtons();
     document.querySelector('#restart-button').addEventListener('click',() => location.reload());
+
+    const selections = document.querySelectorAll('.selection');
+    selections.forEach((selection) => {
+        selection.addEventListener('click', playOneGame);
+    })
+}
+
+function evalWinner (client, computer) {
+    if (client === computer) {
+        console.log(`Draw. Computer also chooses ${client}.`)
+    } else {
+        if ((client === 'rock' && computer === 'paper') ||
+            (client === 'paper' && computer === 'scissors') ||
+            (client === 'scissors' && computer === 'rock')) {
+                console.log(`You lose because ${computer} beats ${client}`);
+            }
+        else if ((client === 'rock' && computer === 'scissors') ||
+                 (client === 'paper' && computer === 'rock') ||
+                 (client === 'scissors' && computer === 'paper')) {
+                    console.log(`You won because ${client} beats ${computer}`)
+                 }
+    }
+}
+
+function playOneGame(event) {
+    let playerChoice = event.target.textContent.toLowerCase();
+    let computerChoice = getComputerChoice();
+    evalWinner(playerChoice, computerChoice);
 }
