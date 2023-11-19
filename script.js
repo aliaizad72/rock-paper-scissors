27cf5c8a628233391db2cc7ad81ea1714514e05c
Ali Aizad <aliaizad72@gmail.com>
2023-11-19 13:03:07 +0800

    Craete a fn that remove btn and add selections

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function getComputerChoice () {
-    const rps = ["rock", "paper", "scissors"];  
-    const pick = Math.floor(Math.random()*rps.length); 
-    return rps[pick]; 
+// function getComputerChoice () {
+//     const rps = ["rock", "paper", "scissors"];  
+//     const pick = Math.floor(Math.random()*rps.length); 
+//     return rps[pick]; 
+// }
+
+const gameButton = document.querySelector('#game-button');
+
+gameButton.addEventListener('click', startGame);
+
+function deleteNode() {
+    gameButton.remove();
 }
 
+function createDivs() {
+    const buttonDiv = document.querySelector('#button-container');
+    const rock = document.createElement('button');
+    buttonDiv.appendChild(rock);
+}
+
+
+function startGame() {
+    deleteNode();
+    createDivs();
+}
